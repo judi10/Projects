5e8861ebc2937d2da5f570ac61f88e81efcef139
judi10 <94863355+judi10@users.noreply.github.com>
2025-01-30 11:06:18 -0500

    Add files via upload

---
+++ jeu Sudoku/jeuSudoku.py
@@ -0,0 +1,69 @@
+﻿def verifierLigne(grille, row, num):
+    '''
+        (list, int, int) -> Bool
+        Vérifier si la case à ajouter n'existe pas sur la ligne.
+        Preconditions: grille est une reference a une matrice 9x9 qui contient déja des nombres de 1 à 9
+    '''
+    # A COMPLETER
+    # verifier si le numero est deja present dans la ligne 
+    return num not in grille[row]
+
+def verifierCol(grille, col, num):
+    '''
+            (list, int, int) -> Bool
+            Vérifier si la case à ajouter n'existe pas sur la colonne.
+            Preconditions: grille est une reference a une matrice 9x9 qui contient déja des nombres de 1 à 9
+    '''
+    # A COMPLETER
+    for row in range(9):
+        if grille[row][col] == num:
+            return True
+    return False
+
+def verifierSousGrille(grille, row, col, num):
+    '''
+            (list, int, int) -> Bool
+            Vérifier si la case à ajouter n'existe pas sur la sous-grille.
+            Preconditions: grille est une reference a une matrice 9x9 qui contient déja des nombres de 1 à 9
+    '''
+
+    # A COMPLETER
+    debutRow= (row//3)*3
+    debutcol = (col//3)*3
+
+    for i in range(debutRow, debutRow +3):
+        for j in range(debutcol, debutcol +3):
+            if grille[i][j] == num:
+                return False
+    return True
+
+def verifierGagner(grille):
+    '''(list) ->  bool
+    * Preconditions: grille est une reference a une matrice 9x9 qui contient de nombres de 1 à 9
+    * Verifie si la grille est completement remplie.
+    '''
+    for i in range(len(grille)):
+        for j in range(len(grille[0])):
+            if grille[i][j] == 0:
+                return False
+    return True
+    
+   # A COMPLETER
+  
+def verifierValide(grille, row, col, num):
+   ''' (list, int, int, int) ->  bool
+   * verifie si le nombre proposé est bon sur la ligne et colonne et la sous-grille donné en parametre.
+   * Preconditions: tab est une reference a une matrice 9 x 9 qui contient des chiffres entre 1 et 9
+   '''
+
+   
+   # A COMPLETER
+
+   a = verifierCol(grille, col, num)
+   b = verifierSousGrille(grille, row, col, num)
+   c = verifierLigne(grille, row, num)
+   if not(a and b and c):
+       return False
+   return True
+   
+
